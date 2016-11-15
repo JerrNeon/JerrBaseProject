5d83b74ca85d903ffdce724538dcbf9b2e999faa
JerrNeon <c1174051244@163.com>
2016-11-15 11:37:08 +0800

    modify

--- AndoridBasePro/app/src/main/java/com/cw/andoridmvp/activity/MainActivity.java
+++ AndoridBasePro/app/src/main/java/com/cw/andoridmvp/activity/MainActivity.java
@@ -1,15 +1,16 @@
 package com.cw.andoridmvp.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import com.ashokvarma.bottomnavigation.BottomNavigationBar;
-import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.cw.andoridmvp.R;
 import com.cw.andoridmvp.base.activity.BaseTbActivity;
 import com.cw.andoridmvp.fragment.MainFragment;
@@ -23,17 +24,20 @@ import butterknife.BindView;
  * @create by: chenwei
  * @date 2016/8/23 17:11
  */
-public class MainActivity extends BaseTbActivity implements BottomNavigationBar.OnTabSelectedListener {
+public class MainActivity extends BaseTbActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    @BindView(R.id.base_mainBottomBar)
-    BottomNavigationBar mainBottomBar;
+    @BindView(R.id.base_mainBottomView)
+    BottomNavigationView mainBottomView;
 
     /**
      * 再按一次退出程序
      */
     private long exitTime = 0;
+    /**
+     * 当前菜单项
+     */
+    private int currPosition;
 
-    private int[] imgResource = null;
     private String[] strResource = null;
     private Fragment[] mFragments = null;
 
@@ -45,33 +49,10 @@ public class MainActivity extends BaseTbActivity implements BottomNavigationBar.
 
     private void init() {
         mToolIvLeft.setVisibility(View.GONE);
-        imgResource = new int[]{R.drawable.ic_directions_car_white_24dp, R.drawable.ic_directions_car_white_24dp, R.drawable.ic_directions_car_white_24dp};
         strResource = new String[]{"首页", "超低购", "我的"};
         mFragments = new Fragment[]{MainFragment.instance(), MainFragment.instance(), MainFragment.instance()};
-        for (int i = 0; i < imgResource.length; i++) {
-            mainBottomBar.addItem(new BottomNavigationItem(imgResource[i], strResource[i]));
-        }
-        mainBottomBar.initialise();
-        mainBottomBar.setTabSelectedListener(this);
         setDefaultFragment();
-    }
-
-    @Override
-    public void onTabSelected(int position) {
-        setTitleName(strResource[position]);
-        changeFragment(position);
-    }
-
-    @Override
-    public void onTabUnselected(int position) {
-        if (mFragments != null && position < mFragments.length) {
-            getSupportFragmentManager().beginTransaction().remove(mFragments[position]).commitAllowingStateLoss();
-        }
-    }
-
-    @Override
-    public void onTabReselected(int position) {
-
+        mainBottomView.setOnNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -79,17 +60,43 @@ public class MainActivity extends BaseTbActivity implements BottomNavigationBar.
         return R.layout.base_main_layout;
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.main:
+                currPosition = 0;
+                break;
+            case R.id.chaodigou:
+                currPosition = 1;
+                break;
+            case R.id.mine:
+                currPosition = 2;
+                break;
+            default:
+                currPosition = -1;
+                break;
+        }
+        if (currPosition != -1) {
+            changeFragment(currPosition);
+            return true;
+        }
+        return false;
+    }
+
     /**
      * 设置默认的
      */
     private void setDefaultFragment() {
-        setTitleName(strResource[0]);
+        if (strResource != null)
+            setTitleName(strResource[0]);
         if (mFragments != null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.base_mainContent, mFragments[0]).commit();
         }
     }
 
     private void changeFragment(int position) {
+        if (strResource != null)
+            setTitleName(strResource[currPosition]);
         if (mFragments != null) {
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
@@ -109,12 +116,14 @@ public class MainActivity extends BaseTbActivity implements BottomNavigationBar.
     @Override
     public void onBackPressed() {
         if ((System.currentTimeMillis() - exitTime) > 2000) {
-            if (mainBottomBar.getCurrentSelectedPosition() == 0) {
+            if (mainBottomView.getMenu().getItem(0).isChecked()) {
                 Toast.makeText(getApplicationContext(), "再按一次退出程序",
                         Toast.LENGTH_SHORT).show();
                 exitTime = System.currentTimeMillis();
-            } else
-                mainBottomBar.selectTab(0);
+            } else {
+                mainBottomView.getMenu().getItem(0).setChecked(true);
+                mainBottomView.getMenu().getItem(currPosition).setChecked(false);
+            }
         } else {
             super.onBackPressed();
             exit();
