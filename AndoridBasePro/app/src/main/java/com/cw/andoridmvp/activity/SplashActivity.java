a2e91dd09bebf933860ec453e4d81a1f7ada457b
JerrNeon <c1174051244@163.com>
2017-02-16 11:54:33 +0800

    add splashActivity and solve App start empty question

---
+++ AndoridBasePro/app/src/main/java/com/cw/andoridmvp/activity/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.cw.andoridmvp.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.cw.andoridmvp.R;
+
+/**
+ * @version V1.0
+ * @ClassName: ${CLASS_NAME}
+ * @Description: (启动页)
+ * @create by: chenwei
+ * @date 2017/2/16 10:10
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    private Button splash_btn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        init();
+    }
+
+    private void init() {
+        splash_btn = (Button) findViewById(R.id.splash_btn);
+        new CountDownTimer(4000, 1000) {
+            @Override
+            public void onTick(long l) {
+                splash_btn.setText(l / 1000L + "");
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }.start();
+    }
+
+}
